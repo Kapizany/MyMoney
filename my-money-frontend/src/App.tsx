7319448774d77466a71c529c92a73efcbf4874b7
Gabriel Capizani <5434918-gabriel.capizani@users.noreply.gitlab.com>
2022-01-06 19:33:27 -0300

    Start login page

--- my-money-frontend/src/App.tsx
+++ my-money-frontend/src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button, ChakraProvider, Container, Icon, Input, Link, Heading } from '@chakra-ui/react'
+import { theme } from './styles/theme';
+import { FaDollarSign } from 'react-icons/fa';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <ChakraProvider theme={theme}>
+    <Box w="xl" mx="auto" mt="6rem" bgColor="gray.50">
+      <Container pt="2rem" pb="2rem">
+        <Box textAlign="center" color="dollar.500">
+          <Icon boxSize="3.5rem" as={FaDollarSign} />
+          <Heading fontSize="1.4rem">
+          M<Box fontSize="1.2rem" display="inline" color="dollar.900">y</Box>M<Box fontSize="1.2rem" display="inline" color="dollar.900">oney</Box>
+          </Heading>
+        </Box>
+        
+      </Container>
+      <Container>
+        <Input placeholder='Email' />
+      </Container>
+      <Container>
+        <Input placeholder='Senha' />
+      </Container>
+      <Container><Button bgColor="dollar.500">LOGIN</Button></Container>
+      <Container><Link>Esqueceu?</Link></Container>
+    </Box>
+  </ChakraProvider> 
 }
 
 export default App;
---
+++ my-money-frontend/src/styles/theme.ts
@@ -0,0 +1,42 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+    colors: {
+        gray: {
+            "900": "#181b23",
+            "800": "#1f2029",
+            "700": "#353646",
+            "600": "#4b4d63",
+            "500": "#616480",
+            "400": "#797d9a",
+            "300": "#9699b0",
+            "200": "#b3b5c6",
+            "100": "#d1d2dc",
+            "50": "#eeeef2"
+        },
+        dollar: {
+            "900": "#043927",
+            "800": "#2E8B57",
+            "700": "#01796F",
+            "600": "#00A572",
+            "500": "#40ca6e",
+            "400": "#4af784",
+            "300": "#6aefba",
+            "200": "#78e49c",
+            "100": "#98FB98",
+            "50": "#befdbe"
+        }
+    },
+    fonts: {
+        heading: "Roboto",
+        body: "Roboto",
+    },
+    styles: {
+        global: {
+            body: {
+                bg: 'gray.800',
+                color:'gray.50'
+            }
+        }
+    }
+});
