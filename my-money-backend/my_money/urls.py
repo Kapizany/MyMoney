43f31281d1b84e44b6484eb26325240e85cac184
Gabriel Capizani <5434918-gabriel.capizani@users.noreply.gitlab.com>
2022-01-10 23:24:24 -0300

    Create authentication

--- my-money-backend/my_money/urls.py
+++ my-money-backend/my_money/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.authtoken import views
+from rest_framework import routers
+from transactions.views import UserViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
