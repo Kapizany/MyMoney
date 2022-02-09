06fcccae02d531b5e9e1d5fa29cca1a8fe13ba6e
Gabriel Capizani <5434918-gabriel.capizani@users.noreply.gitlab.com>
2022-02-09 18:44:36 -0300

    Fix transaction screen and update transaction and people queryset

--- my-money-backend/mixers/transactions_mixer.py
+++ my-money-backend/mixers/transactions_mixer.py
@@ -1,4 +1,4 @@
 from mixer.backend.django import mixer
 from transactions.models import Transaction
 
-mixer.cycle(100).blend(Transaction)
+mixer.cycle(100).blend(Transaction, user__password='pbkdf2_sha256$320000$60Eqz8lvPWZ06VRL2tLnxL$JQKOBkgE2XgkyZZCEW6K7nXW9NbBy2pcF4gI914WE94=')
--- my-money-frontend/src/styles/theme.ts
+++ my-money-frontend/src/styles/theme.ts
@@ -34,7 +34,7 @@ export const theme = extendTheme({
     styles: {
         global: {
             body: {
-                bg: 'gray.900',
+                bg: 'gray.100',
                 color:'gray.50'
             }
         }
