17647281b86ddc1f74fa40dd495d6a2812c20e6c
Gabriel Capizani <5434918-gabriel.capizani@users.noreply.gitlab.com>
2022-02-21 18:04:04 -0300

    Backend - Transactions action that returns monthly values given a year

--- my-money-backend/transactions/views.py
+++ my-money-backend/transactions/views.py
@@ -1,7 +1,9 @@
+from django.db.models import Sum
+from datetime import date
 from rest_framework import viewsets
-from .serializers import PersonSerializer, TransactionSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from .serializers import PersonSerializer, TransactionSerializer
 from .models import Person, Transaction
 
 
@@ -32,3 +34,31 @@ class TransactionViewSet(viewsets.ModelViewSet):
         if self.request.user.is_superuser:
             return Transaction.objects.all()
         return Transaction.objects.filter(user=self.request.user).all()
+    
+    @action(detail=False, methods=['get'], name="Get months values for a year")
+    def get_months_values(self, request):
+        year = date.today().year
+        if request.query_params.get('year'):
+            year = request.query_params.get('year')
+        
+        months = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"]
+        results = {}
+        for index, month in enumerate(months):
+            credit = Transaction.objects.filter(user=request.user,date__month=index+1, date__year=year, value__gte=0).aggregate(Sum('value'))['value__sum']
+            debit = Transaction.objects.filter(user=request.user,date__month=index+1, date__year=year, value__lte=0).aggregate(Sum('value'))['value__sum']
+            if not credit:
+                credit = 0
+            if not debit:
+                debit = 0
+            last_month_cumulative_balance = 0
+            if index > 0:
+                last_month_cumulative_balance = results[months[index-1]]['cumulative_balance']
+            results[month] = {
+                "credit":credit,
+                "debit":debit,
+                "balance":credit+debit,
+                "cumulative_balance":last_month_cumulative_balance + credit + debit,
+            }
+
+        return Response(results)
