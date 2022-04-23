72ef6b30268a722e3cee15b134f81e66d95a5690
Gabriel Capizani <5434918-gabriel.capizani@users.noreply.gitlab.com>
2022-04-22 22:54:32 -0300

    Create a backend dockerfile and docker-compose service

--- my-money-backend/my_money/settings.py
+++ my-money-backend/my_money/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'my_money.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "mymoneydb",
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db',
+        'PORT': '3307',
     }
 }
 
